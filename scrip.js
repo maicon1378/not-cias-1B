a8854dfc22762bfea3395bce95821d6b6c1fa725
maicon alves <maiconnascimento2007@gmail.com>
2024-04-02 10:53:08 -0300

    criação da navegação desktop, remoção da classe header-mobile e inicio da responsividade

---
+++ scrip.js
@@ -0,0 +1,21 @@
+const botaoAbrir =  document.querySelector("header > button")
+const botaoFechar = document.querySelector("header nav button")
+
+const body = document.querySelector("body")
+const nav = document.querySelector("header .mobile")
+
+console.log(body)
+
+botaoAbrir.addEventListener('click', abrirMenu)
+botaoFechar.addEventListener('click', fecharMenu )
+
+function abrirMenu() {
+    body.classList.add("escurecer")
+    nav.classList.add("abrir")
+
+}
+
+function fecharMenu() {
+    body.classList.remove("escurecer")
+    nav.classList.remove("abrir")
+}
